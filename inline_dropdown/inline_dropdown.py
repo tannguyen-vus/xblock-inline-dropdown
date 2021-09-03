3463cd519026024457a00e61a0f50553158873e0
tannguyen-vus <83755572+tannguyen-vus@users.noreply.github.com>
2021-09-03 21:12:33 +0700

    Update inline_dropdown.py

--- inline_dropdown/inline_dropdown.py
+++ inline_dropdown/inline_dropdown.py
@@ -47,25 +47,7 @@ class InlineDropdownXBlock(XBlock):
         scope=Scope.content,
         default=textwrap.dedent('''
             <inline_dropdown schema_version='1'>
-                <body>
-                    <p>A fruit is the fertilized ovary of a tree or plant and contains seeds. Given this, a <input_ref input="i1"/> is consider a fruit, while a <input_ref input="i2"/> is considered a vegetable.</p>
-                </body>
-                <optionresponse>
-                	<optioninput id="i1">
-                		<option correct="True">tomato<optionhint>Since the tomato is the fertilized ovary of a tomato plant and contains seeds, it is a fruit.</optionhint></option>
-                		<option correct="False">potato<optionhint>A potato is an edible part of a plant in tuber form and is a vegetable, not a fruit.</optionhint></option>
-                	</optioninput>
-                </optionresponse>
-                <optionresponse>
-                	<optioninput id="i2">
-                		<option correct="False">cucumber<optionhint>Many people mistakenly think a cucumber is a vegetable. However, because a cucumber is the fertilized ovary of a cucumber plant and contains seeds, it is a fruit.</optionhint></option>
-                		<option correct="True">onion<optionhint>The onion is the bulb of the onion plant and contains no seeds and is therefore a vegetable.</optionhint></option>
-                	</optioninput>
-                </optionresponse>
-                <demandhint>
-                    <hint>A fruit is the fertilized ovary from a flower.</hint>
-                    <hint>A fruit contains seeds of the plant.</hint>
-                </demandhint>
+               
             </inline_dropdown>
         '''))
 
