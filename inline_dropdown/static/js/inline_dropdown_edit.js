b7b106ca6d2a436badc4641e6e4156b866d58fe4
tannguyen-vus <83755572+tannguyen-vus@users.noreply.github.com>
2021-09-10 12:41:45 +0700

    Update inline_dropdown_edit.js

--- inline_dropdown/static/js/inline_dropdown_edit.js
+++ inline_dropdown/static/js/inline_dropdown_edit.js
@@ -1,9 +1,9 @@
 /* Javascript for Inline Dropdown XBlock. */
 function InlineDropdownXBlockInitEdit(runtime, element) {
     console.log(element);
-    var xmlEditorTextarea = $('.block-xml-editor', element),
-      //  xmlEditor = CodeMirror.fromTextArea(xmlEditorTextarea[0], { mode: 'xml', lineWrapping: true });
-    //console.log(xmlEditor);
+    var xmlEditorTextarea = $('.block-xml-editor', element);
+      xmlEditor = CodeMirror.fromTextArea(xmlEditorTextarea[0], { mode: 'xml', lineWrapping: true });
+    console.log(xmlEditor);
     $(element).find('.action-cancel').bind('click', function() {
         runtime.notify('cancel', {});
     });
